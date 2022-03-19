ca18d3dfcf82d7710e3472ec8dbc0e67bd2c1ac6
theautomation <github@theautomation.nl>
2022-03-19 13:14:16 +0100

    update cicd pipelines

--- healthcheck/healthcheck_sonarr.sh
+++ healthcheck/healthcheck_sonarr.sh
@@ -2,7 +2,7 @@
 
 FILE=/config/config.xml
 
-until test -f $FILE; do sleep 1; done
+until test -f $FILE; do sleep 5; done
 
 API=$(xml_grep --text_only 'ApiKey' $FILE)
 curl -f "http://localhost:8989/api/system/status?apikey=$API" && curl api.ipify.org || exit 1
